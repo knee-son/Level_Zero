29be38538f4f3348e97161cbb4c141df8b768d03
knee-son <okaokaloid@gmail.com>
2023-02-05 21:14:06 +0800

    nakuha ra ang strings sa email and password. ibalhin ang sign in method sa RegisterActivity. fix warnings lang use @string

--- app/src/main/java/com/level_zero/greeniq/RegisterActivity.java
+++ app/src/main/java/com/level_zero/greeniq/RegisterActivity.java
@@ -3,11 +3,17 @@ package com.level_zero.greeniq;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.level_zero.greeniq.databinding.ActivityMainBinding;
+import com.level_zero.greeniq.databinding.ActivityRegisterBinding;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,9 +22,23 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        com.level_zero.greeniq.databinding.ActivityRegisterBinding binding
+                = ActivityRegisterBinding.inflate(getLayoutInflater());
+
         setContentView(R.layout.activity_register);
+
+        binding.signupButton.setOnClickListener (v -> {
+            String email = Objects.requireNonNull(binding.userEmail.getText()).toString();
+            String password = Objects.requireNonNull(binding.userPassword.getText()).toString();
+//
+//                mAuth = FirebaseAuth.getInstance();
+//                mUser = mAuth.getCurrentUser();
+
+//                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password);
+        });
     }
 }
--- app/src/main/java/com/level_zero/greeniq/ui/home/HomeFragment.java
+++ app/src/main/java/com/level_zero/greeniq/ui/home/HomeFragment.java
@@ -1,11 +1,9 @@
 package com.level_zero.greeniq.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,7 +18,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.level_zero.greeniq.databinding.FragmentHomeBinding;
-import com.level_zero.greeniq.ui.dashboard.DashboardFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -34,41 +31,34 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        email = binding.userEmail;
-        password = binding.userPassword;
-        signUp = binding.createButton;
 
-        mAuth = FirebaseAuth.getInstance();
-        mUser = mAuth.getCurrentUser();
-
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerUser();
-            }
-        });
+//        signUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                registerUser();
+//            }
+//        });
 
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
-    private void registerUser() {
-        String userEmail = email.toString().trim();
-        String userPass = password.toString().trim();
-
-        mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(getContext(),"Registration is Complete", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(getContext(),"Registration is not Complete", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
+//    private void registerUser() {
+//        String userEmail = email.toString().trim();
+//        String userPass = password.toString().trim();
+//
+//        mAuth.createUserWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if(task.isSuccessful()){
+//                    Toast.makeText(getContext(),"Registration is Complete", Toast.LENGTH_SHORT).show();
+//                }else {
+//                    Toast.makeText(getContext(),"Registration is not Complete", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
 
     @Override
     public void onDestroyView() {
