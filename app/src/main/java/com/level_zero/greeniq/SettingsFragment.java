79e6822c191376afb0b552c9c173a0689c0c1a3e
notaltt <cabanerojeb@gmail.com>
2023-03-29 21:03:31 +0800

    added dialog for about application

--- app/src/main/java/com/level_zero/greeniq/SettingsFragment.java
+++ app/src/main/java/com/level_zero/greeniq/SettingsFragment.java
@@ -2,6 +2,9 @@ package com.level_zero.greeniq;
 
 import static androidx.navigation.Navigation.findNavController;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -13,10 +16,13 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.level_zero.greeniq.databinding.FragmentAirQualityBinding;
@@ -35,6 +41,33 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
 
         back = binding.back1;
+        binding.aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View customLayout = getLayoutInflater().inflate(R.layout.dialog_custom, null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                TextView titleView = customLayout.findViewById(R.id.dialog_title);
+                TextView messageView = customLayout.findViewById(R.id.dialog_message);
+                AppCompatButton okButton = customLayout.findViewById(R.id.button);
+
+                titleView.setText("About Application");
+                messageView.setText("[Test Details about the application]");
+
+
+                builder.setView(customLayout);
+                AlertDialog dialog = builder.create();
+
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
 
         binding.logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
